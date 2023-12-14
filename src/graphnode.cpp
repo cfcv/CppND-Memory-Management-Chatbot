e2d35814e034ec673903532ff7d86924bf7a744e
Carlos Valadares <carlosfernando.coelhovaladares@stellantis.com>
2023-12-14 11:59:53 +0100

    task 1 ad 3

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // chatbot is already deleted in chatlogic destructor
+    //delete _chatBot; 
 
     ////
     //// EOF STUDENT CODE
